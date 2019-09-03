587acf742304599fb9b94fd7e451eaf5d2f4958c
Marat Akhin <Marat.Akhin@gmail.com>
2019-09-03 15:35:31 +0300

    Update List.kt

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -122,7 +122,7 @@ fun abs(v: List<Double>): Double = TODO()
  *
  * Рассчитать среднее арифметическое элементов списка list. Вернуть 0.0, если список пуст
  */
-fun mean(list: List<Double>): Double = TODO()
+fun mean(list: List<Double>): Double = list.sum() / list.size
 
 /**
  * Средняя
